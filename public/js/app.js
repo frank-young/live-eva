ce2c9580d13f1586450d96d118703434c4a645d7
frank <yangjunalns@qq.com>
2017-03-08 19:12:36 +0800

    dev

--- public/js/app.js
+++ public/js/app.js
@@ -11233,8 +11233,9 @@ module.exports = g;
 
 /***/ }),
 /* 11 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
 
 /**
  * First we will load all of this project's JavaScript dependencies which
